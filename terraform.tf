2cdecdc0f89e023563f67c44abebef130235277f
Pedro Urdaibay <purdaibay@g-p.com>
2024-11-20 16:47:15 +0000

    CE-21347 - Updated runtime

--- terraform.tf
+++ terraform.tf
@@ -240,7 +240,7 @@ resource "aws_lambda_function" "ingestion_function" {
     ? var.function_role
     : aws_iam_role.lambda_role.0.arn
   )
-  runtime     = "python3.11"
+  runtime     = "python3.13"
   filename    = local.archive_name
   handler     = "function.lambda_handler"
   memory_size = var.memory_size
